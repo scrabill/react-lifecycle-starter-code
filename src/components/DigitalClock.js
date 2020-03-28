a6f1dca8dc41cf65b514965a0a124a2bd28207da
scrabill <shannon.crabill@gmail.com>
2020-03-28 16:25:17 -0400

    Working clock that updates state and displayed time each second

--- src/components/DigitalClock.js
+++ src/components/DigitalClock.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react'
 
 class DigitalClock extends Component {
 
+  state = {
+    time: new Date()
+  }
+
+  componentDidMount() {
+    setInterval(this.keepTime, 1000)
+  }
+
+  keepTime = () => {
+    this.setState({
+      time: new Date()
+    })
+  }
+
+  // TODO: Add AM or PM for non-military time
+
   render () {
     return (
       <div className="app-children">
         <h2 id="digital">
-          11:00 AM
+          {this.state.time.getHours()}:{this.state.time.getMinutes()}:{this.state.time.getSeconds()}
         </h2>
       </div>
     )
